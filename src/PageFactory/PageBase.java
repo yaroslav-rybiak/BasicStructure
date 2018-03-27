0a219223d8fa6e45fadba94e91282975c4b310c5
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2018-03-27 18:45:22 +0200

    wait moved to testbase

--- src/PageFactory/PageBase.java
+++ src/PageFactory/PageBase.java
@@ -1,11 +1,9 @@
 package PageFactory;
 
 import Tests.TestBase;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract class PageBase extends TestBase {
 
-    WebDriverWait wait = new WebDriverWait(driver, 10);
     String URL;
 
     public void open() {
--- src/Tests/TestBase.java
+++ src/Tests/TestBase.java
@@ -3,6 +3,7 @@ package Tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 abstract public class TestBase {
     protected static WebDriver driver;
+    protected static WebDriverWait wait;
 
     @BeforeSuite
     public void start() {
@@ -17,6 +19,7 @@ abstract public class TestBase {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterSuite
